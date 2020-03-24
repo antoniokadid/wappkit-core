7fee4b2c9d1ba256153575e789c59a72b4035e8f
Antonio Kadid <antoniokadid@gmail.com>
2020-03-24 19:28:18 +0200

    Introduction of an engine that allows content to be generated from user code

---
+++ src/Exceptions/IOException.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace AntonioKadid\WAPPKitCore\Exceptions;
+
+
+use Exception;
+
+/**
+ * Class IOException
+ *
+ * @package AntonioKadid\WAPPKitCore\Exceptions
+ */
+class IOException extends Exception
+{
+}
