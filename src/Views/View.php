81f96bd175e35f72726ef3dfd0994a03a9df27f4
Antonio Kadid <antoniokadid@gmail.com>
2020-11-26 13:29:40 +0200

    Updated View to include SEO attributes

--- src/Views/View.php
+++ src/Views/View.php
@@ -12,7 +12,91 @@ use AntonioKadid\WAPPKitCore\HTTP\Response\HTMLResponse;
 abstract class View extends HTMLResponse
 {
     /** @var string */
-    public $description;
+    private $description;
+    /** @var string[] */
+    private $keywords = [];
     /** @var string */
-    public $title;
+    private $title;
+    /** @var bool instructs the search engine crawler to index or not to index a page. */
+    private $robotIndex = true;
+    /** @var bool instructs the search engine crawler to follow or not to follow any links on a page. */
+    private $robotFollow = true;
+
+    /**
+     * @param string $title
+     *
+     * @return View
+     */
+    public function setTitle(string $title): View
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @param string $description
+     *
+     * @return View
+     */
+    public function setDescription(string $description): View
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @param string $keyword
+     *
+     * @return View
+     */
+    public function addKeyword(string $keyword): View
+    {
+        if (!in_array($keyword, $this->keywords)) {
+            array_push($this->keywords, $keyword);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return View
+     */
+    public function disableSearchEngineIndexing(): View
+    {
+        $this->robotIndex = false;
+
+        return $this;
+    }
+
+    /**
+     * @return View
+     */
+    public function enableSearchEngineIndexing(): View
+    {
+        $this->robotIndex = true;
+
+        return $this;
+    }
+
+    /**
+     * @return View
+     */
+    public function enableSearchEngineLinkFollow(): View
+    {
+        $this->robotFollow = true;
+
+        return $this;
+    }
+
+    /**
+     * @return View
+     */
+    public function disableSearchEngineLinkFollow(): View
+    {
+        $this->robotFollow = false;
+
+        return $this;
+    }
 }
