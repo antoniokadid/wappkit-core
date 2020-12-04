60c04530535c27e452653724a60afd3092b443d1
Antonio Kadid <antoniokadid@gmail.com>
2020-12-04 23:28:57 +0200

    Add value to flag

--- src/Flag.php
+++ src/Flag.php
@@ -13,6 +13,17 @@ class Flag
      * @param int $flags
      * @param int $flag
      *
+     * @return int
+     */
+    public static function add(int $flags, int $flag): int
+    {
+        return $flags | $flag;
+    }
+
+    /**
+     * @param int $flags
+     * @param int $flag
+     *
      * @return bool
      */
     public static function exists(int $flags, int $flag): bool
