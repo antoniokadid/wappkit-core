b1b0f068f40ea6b18a474ea5bb85bff25432d4dc
Antonio Kadid <antoniokadid@gmail.com>
2020-12-05 10:06:30 +0200

    Consolidation of text cases

--- src/Text/TextCase.php
+++ src/Text/TextCase.php
@@ -7,66 +7,306 @@ namespace AntonioKadid\WAPPKitCore\Text;
  *
  * @package AntonioKadid\WAPPKitCore\Text
  */
-abstract class TextCase
+class TextCase
 {
+    public const CAMEL_SNAKE           = 1;
+    public const FLAT                  = 2;
+    public const KEBAB                 = 3;
+    public const LOWER_CAMEL           = 4;
+    public const SCREAMING_KEBAB       = 5;
+    public const SCREAMING_SNAKE       = 6;
+    public const SNAKE                 = 7;
+    public const TRAIN                 = 8;
+    public const UNKNOWN               = 0;
+    public const UPPER_CAMEL           = 9;
+    public const UPPER_FLAT            = 10;
+
     /** @var string[] */
-    protected $parts = [];
+    private $parts = [];
+    /** @var string */
+    private $text;
 
     /**
-     * @param string $input
+     * @param string $text
      */
-    public function load(string $input): void
+    public function __construct(string $text)
     {
-        if ($this->valid($input)) {
-            $this->parts = $this->dismantle($input);
-
-            return;
-        }
-
-        $this->parts = [];
-
-        $parts = preg_split('/\s+/', $input, -1, PREG_SPLIT_NO_EMPTY);
-        if ($parts === false) {
-            return;
-        }
-
-        foreach ($parts as $part) {
-            array_push($this->parts, ...$this->dismantle($this->sanitize($part)));
-        }
+        $this->text  = $text;
+        $this->parts = self::dismantle($text);
     }
 
     /**
-     * @param TextCase $textCase
+     * Attempt to detect a valid text case.
+     *
+     * @param string $text
+     *
+     * @return int
      */
-    public function loadFromTextCase(TextCase $textCase): void
+    public static function identify(string $text): int
     {
-        $this->parts = $textCase->parts;
+        return
+            self::isLowerCamel($text) ? self::LOWER_CAMEL :
+            (self::isCamelSnake($text) ? self::CAMEL_SNAKE :
+            (self::isFlat($text) ? self::FLAT :
+            (self::isKebab($text) ? self::KEBAB :
+            (self::isScreamingKebab($text) ? self::SCREAMING_KEBAB :
+            (self::isScreamingSnake($text) ? self::SCREAMING_SNAKE :
+            (self::isSnake($text) ? self::SNAKE :
+            (self::isTrain($text) ? self::TRAIN :
+            (self::isUpperCamel($text) ? self::UPPER_CAMEL :
+            (self::isUpperFlat($text) ? self::UPPER_FLAT :
+            self::UNKNOWN)))))))));
     }
 
     /**
-     * Take apart the $input string based on the underlying text case.
-     *
-     * @param string $input
-     *
-     * @return array
-     */
-    abstract protected function dismantle(string $input): array;
-
-    /**
-     * Sanitize unknown characters based on the underlying text case.
-     *
-     * @param string $input
-     *
-     * @return string
-     */
-    abstract protected function sanitize(string $input): string;
-
-    /**
-     * Check if the $input is valid based on the underlying text case.
-     *
-     * @param string $input
+     * @param string $text
      *
      * @return bool
      */
-    abstract protected function valid(string $input): bool;
+    public static function isCamelSnake(string $text): bool
+    {
+        return preg_match('/^([[:upper:]][0-9[:lower:]]+_?)+_[[:upper:]][0-9[:lower:]]+$/', $text) === 1;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return bool
+     */
+    public static function isFlat(string $text): bool
+    {
+        return preg_match('/^[0-9[:lower:]]+$/', $text) === 1;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return bool
+     */
+    public static function isKebab(string $text): bool
+    {
+        return preg_match('/^([0-9[:lower:]]+-?)+-[0-9[:lower:]]+$/', $text) === 1;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return bool
+     */
+    public static function isLowerCamel(string $text): bool
+    {
+        return preg_match('/^([[:lower:]]+[0-9[:lower:]]*[[:upper:]][0-9[:lower:]]*)+$/', $text) === 1;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return bool
+     */
+    public static function isScreamingKebab(string $text): string
+    {
+        return preg_match('/^([0-9[:upper:]]+-?)+-[0-9[:upper:]]+$/', $text) === 1;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return bool
+     */
+    public static function isScreamingSnake(string $text): string
+    {
+        return preg_match('/^([0-9[:upper:]]+_?)+_[0-9[:upper:]]+$/', $text) === 1;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return bool
+     */
+    public static function isSnake(string $text): string
+    {
+        return preg_match('/^([0-9[:lower:]]+_?)+_[0-9[:lower:]]+$/', $text) === 1;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return bool
+     */
+    public static function isTrain(string $text): string
+    {
+        return preg_match('/^([[:upper:]][0-9[:lower:]]+-?)+-[[:upper:]][0-9[:lower:]]+$/', $text) === 1;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return bool
+     */
+    public static function isUpperCamel(string $text): string
+    {
+        return preg_match('/^([[:upper:]][0-9[:lower:]]+)+[[:upper:]]?$/', $text) === 1;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return bool
+     */
+    public static function isUpperFlat(string $text): string
+    {
+        return preg_match('/^[0-9[:upper:]]+$/', $text) === 1;
+    }
+
+    /**
+     * Convert to camelCase.
+     *
+     * @return string
+     */
+    public function toCamelCase(): string
+    {
+        $tmp = $this->parts;
+
+        $first = array_shift($tmp);
+        if ($first == null) {
+            return '';
+        }
+
+        return array_reduce($tmp, function (string $carry, string $part) {
+            return $carry . ucfirst(strtolower($part));
+        }, strtolower($first));
+    }
+
+    /**
+     * Convert to Camel_Snake_Case.
+     *
+     * @return string
+     */
+    public function toCamelSnakeCase(): string
+    {
+        return implode('_', array_map(function (string $part) {
+            return ucfirst(strtolower($part));
+        }, $this->parts));
+    }
+
+    /**
+     * Convert to flatcase.
+     *
+     * @return string
+     */
+    public function toFlatCase(): string
+    {
+        return strtolower(implode('', $this->parts));
+    }
+
+    /**
+     * Convert to kebab-case.
+     *
+     * @return string
+     */
+    public function toKebabCase(): string
+    {
+        return strtolower(implode('-', $this->parts));
+    }
+
+    /**
+     * Convert to SCREAMING-KEBAB-CASE.
+     *
+     * @return string
+     */
+    public function toScreamingKebabCase(): string
+    {
+        return strtoupper(implode('-', $this->parts));
+    }
+
+    /**
+     * Convert to CAMEL_SCREAMING_CASE.
+     *
+     * @return string
+     */
+    public function toScreamingSnakeCase(): string
+    {
+        return strtoupper(implode('_', $this->parts));
+    }
+
+    /**
+     * Convert to snake-case.
+     *
+     * @return string
+     */
+    public function toSnakeCase(): string
+    {
+        return strtolower(implode('_', $this->parts));
+    }
+
+    /**
+     * Convert to Train-Case.
+     *
+     * @return string
+     */
+    public function toTrainCaseCase(): string
+    {
+        return implode('-', array_map(function (string $part) {
+            return ucfirst(strtolower($part));
+        }, $this->parts));
+    }
+
+    /**
+     * Convert to UpperCamelCase.
+     *
+     * @return string
+     */
+    public function toUpperCamelCase(): string
+    {
+        return array_reduce($this->parts, function (string $carry, string $part) {
+            return $carry . ucfirst(strtolower($part));
+        }, '');
+    }
+
+    /**
+     * Convert to UPPERFLATCASE.
+     *
+     * @return string
+     */
+    public function toUpperFlatCase(): string
+    {
+        return strtoupper(implode('', $this->parts));
+    }
+
+    /**
+     * Attempt to take apart a string.
+     *
+     * @param string $text
+     *
+     * @return string[]
+     */
+    private static function dismantle(string $text): array
+    {
+        $case = self::identify($text);
+
+        switch ($case) {
+            case TextCase::SNAKE:
+            case TextCase::CAMEL_SNAKE:
+            case TextCase::SCREAMING_SNAKE:
+                return explode('_', $text);
+            case TextCase::KEBAB:
+            case TextCase::SCREAMING_KEBAB:
+            case TextCase::TRAIN:
+                return explode('-', $text);
+            case TextCase::FLAT:
+            case TextCase::UPPER_FLAT:
+                return [$text];
+            case TextCase::LOWER_CAMEL:
+            case TextCase::UPPER_CAMEL:
+                $result = preg_replace('/([[:upper:]])/', "\n$1", $text);
+                if ($result == null) {
+                    return [];
+                }
+
+                return explode("\n", trim($result));
+            default:
+                $parts = preg_split('/\s+/', $text, -1, PREG_SPLIT_NO_EMPTY);
+                return $parts === false ? [] : $parts;
+        }
+    }
 }
--- tests/Text/TextCaseTest.php
+++ tests/Text/TextCaseTest.php
@@ -2,20 +2,112 @@
 
 namespace AntonioKadid\WAPPKitCore\Tests\Text;
 
-use AntonioKadid\WAPPKitCore\Text\CamelCase;
-use AntonioKadid\WAPPKitCore\Text\SnakeCase;
+use AntonioKadid\WAPPKitCore\Text\TextCase;
+use function PHPUnit\Framework\assertEquals;
+
 use PHPUnit\Framework\TestCase;
 
 class TextCaseTest extends TestCase
 {
-    public function testLoadFromTextCase()
+    public function testIdentify()
     {
-        $snakeCase = new SnakeCase();
-        $snakeCase->load('this_is_snake_case_input');
+        $text = 'antonioKadidIsTesting';
+        assertEquals(TextCase::LOWER_CAMEL, TextCase::identify($text));
 
-        $camelCase = new CamelCase();
-        $camelCase->loadFromTextCase($snakeCase);
+        $text = 'Antonio_Kadid_Is_Testing';
+        assertEquals(TextCase::CAMEL_SNAKE, TextCase::identify($text));
 
-        $this->assertEquals('ThisIsSnakeCaseInput', $camelCase->toUpperCamelCase());
+        $text = 'antoniokadidistesting';
+        assertEquals(TextCase::FLAT, TextCase::identify($text));
+
+        $text = 'antonio-kadid-is-testing';
+        assertEquals(TextCase::KEBAB, TextCase::identify($text));
+
+        $text = 'ANTONIO-KADID-IS-TESTING';
+        assertEquals(TextCase::SCREAMING_KEBAB, TextCase::identify($text));
+
+        $text = 'ANTONIO_KADID_IS_TESTING';
+        assertEquals(TextCase::SCREAMING_SNAKE, TextCase::identify($text));
+
+        $text = 'antonio_kadid_is_testing';
+        assertEquals(TextCase::SNAKE, TextCase::identify($text));
+
+        $text = 'Antonio-Kadid-Is-Testing';
+        assertEquals(TextCase::TRAIN, TextCase::identify($text));
+
+        $text = 'AntonioKadidIsTesting';
+        assertEquals(TextCase::UPPER_CAMEL, TextCase::identify($text));
+
+        $text = 'ANTONIOKADIDISTESTING';
+        assertEquals(TextCase::UPPER_FLAT, TextCase::identify($text));
+
+        $text = 'Antonio@Kadid--Is-Testing';
+        assertEquals(TextCase::UNKNOWN, TextCase::identify($text));
+    }
+
+    public function testToCamelCase()
+    {
+        $case = new TextCase("Test\ncamel\ncase");
+        assertEquals('testCamelCase', $case->toCamelCase());
+
+        $case = new TextCase("Test\nc@mel\nc@se");
+        assertEquals('testC@melC@se', $case->toCamelCase());
+    }
+
+    public function testToCamelSnakeCase()
+    {
+        $case = new TextCase('test camel snake case');
+        assertEquals('Test_Camel_Snake_Case', $case->toCamelSnakeCase());
+    }
+
+    public function testToFlatCase()
+    {
+        $case = new TextCase('Test_Flat_Case');
+        assertEquals('testflatcase', $case->toFlatCase());
+
+        $case = new TextCase('Test_Fl@t_c@se');
+        assertEquals('test_fl@t_c@se', $case->toFlatCase());
+    }
+
+    public function testToKebabCase()
+    {
+        $case = new TextCase('Test-kebab-Case');
+        assertEquals('test-kebab-case', $case->toKebabCase());
+    }
+
+    public function testToScreamingKebabCase()
+    {
+        $case = new TextCase('testScreamingKebabCase');
+        assertEquals('TEST-SCREAMING-KEBAB-CASE', $case->toScreamingKebabCase());
+    }
+
+    public function testToScreamingSnakeCase()
+    {
+        $case = new TextCase('test-camel-snake-case');
+        assertEquals('TEST_CAMEL_SNAKE_CASE', $case->toScreamingSnakeCase());
+    }
+
+    public function testToSnakeCase()
+    {
+        $case = new TextCase('TestCamelSnakeCase');
+        assertEquals('test_camel_snake_case', $case->toSnakeCase());
+    }
+
+    public function testToTrainCase()
+    {
+        $case = new TextCase('test_train_case');
+        assertEquals('Test-Train-Case', $case->toTrainCaseCase());
+    }
+
+    public function testToUpperCamelCase()
+    {
+        $case = new TextCase('test camel case');
+        assertEquals('TestCamelCase', $case->toUpperCamelCase());
+    }
+
+    public function testToUpperFlatCase()
+    {
+        $case = new TextCase('test flat case');
+        assertEquals('TESTFLATCASE', $case->toUpperFlatCase());
     }
 }
