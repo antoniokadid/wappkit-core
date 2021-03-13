6cdf6aedfb620d5d09c71cbd99bd5d637aa18962
Antonio Kadid <antoniokadid@gmail.com>
2021-03-13 09:10:23 +0200

    Attempt to detect from ini file what's the maximum upload size

--- src/IO/File.php
+++ src/IO/File.php
@@ -45,4 +45,51 @@ class File
 
         rmdir($pathname);
     }
+
+    /**
+     * Get the maximum size for file upload.
+     * https://stackoverflow.com/questions/2840755/how-to-determine-the-max-file-upload-limit-in-php.
+     *
+     * @return int
+     */
+    public static function maxUploadSize(): int
+    {
+        //select maximum upload size
+        $maxUpload = self::getBytesFromIniValue(ini_get('upload_max_filesize'));
+        //select post limit
+        $maxPost = self::getBytesFromIniValue(ini_get('post_max_size'));
+        //select memory limit
+        $memoryLimit = self::getBytesFromIniValue(ini_get('memory_limit'));
+        // return the smallest of them, this defines the real limit
+        return min($maxUpload, $maxPost, $memoryLimit);
+    }
+
+    /**
+     * @param string $val
+     *
+     * @return int
+     */
+    private static function getBytesFromIniValue(string $val): int
+    {
+        $matches = [];
+        if (!preg_match('/\s*(\d+)([[:alpha:]])?\s*/', $val, $matches)) {
+            return 0;
+        }
+
+        $size     = intval($matches[1]);
+        $modifier = isset($matches[2]) ? strtolower($matches[2]) : '';
+
+        switch ($modifier) {
+            case 'g':
+                $size *= 1024;
+                // no break
+            case 'm':
+                $size *= 1024;
+                // no break
+            case 'k':
+                $size *= 1024;
+        }
+
+        return $size;
+    }
 }
